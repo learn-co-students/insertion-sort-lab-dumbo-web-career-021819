c820f6fbe24dafe7467e7565785a26857c4fd58f
Nyette <antoinette.lee02@myhunter.cuny.edu>
2019-09-14 20:37:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  // find the smallest element, min
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 1; i < array.length; i++) {
+    if (min > array[i]) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  // remove min from the array
+  array.splice(minIndex, 1);
+  return min;
 }
 
-function insertionSort(array){
-
-}
+function insertionSort(array) {
+  let min;
+  let sortedArray = [];
+  while (array.length !== 0) {
+    min = findMinAndRemove(array);
+    sortedArray.push(min);
+  }
+  return sortedArray;
+ }
